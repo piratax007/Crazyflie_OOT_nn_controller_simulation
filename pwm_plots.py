aabe0a50cbd6c656460df847c24c20ed7ef869e8
Fausto M. Lagos S <piratax007@protonmail.ch>
2025-08-28 23:40:13 +0200

    Order of the plots as the position of the motors

--- pwm_plots.py
+++ pwm_plots.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 firmware_df = pd.read_csv('pwm_log.csv')
+
 gympybullet_pwm0_df = pd.read_csv('gympybullet_data/pwm0-0.csv', header=None, names=['time', 'pwm0'])
 gympybullet_pwm1_df = pd.read_csv('gympybullet_data/pwm1-0.csv', header=None, names=['time', 'pwm1'])
 gympybullet_pwm2_df = pd.read_csv('gympybullet_data/pwm2-0.csv', header=None, names=['time', 'pwm2'])
@@ -12,21 +13,23 @@ firmware_df.columns = ['time', 'pwm0', 'pwm1', 'pwm2', 'pwm3']
 figure, axes = plt.subplots(2, 2, figsize=(12, 8), sharex=True)
 
 gympybullet_dataframes = [
+    gympybullet_pwm3_df,
     gympybullet_pwm0_df,
-    gympybullet_pwm1_df,
     gympybullet_pwm2_df,
-    gympybullet_pwm3_df
+    gympybullet_pwm1_df
 ]
-pwm_columns = ['pwm0', 'pwm1', 'pwm2', 'pwm3']
+
+pwm_columns = ['pwm3', 'pwm0', 'pwm2', 'pwm1']
 
 for idx, ax in enumerate(axes.flatten()):
     col = pwm_columns[idx]
-    individual_df = gympybullet_dataframes[idx]
+    print(f"Plotting {col}")
+    gympybullet_df = gympybullet_dataframes[idx]
 
     ax.plot(firmware_df['time'].values, firmware_df[col].values, label=f'Firmware {col}')
-    ax.plot(individual_df['time'].values, individual_df[col].values, label=f'GymPyBullet {col}', alpha=0.7)
+    ax.plot(gympybullet_df['time'].values, gympybullet_df[col].values, label=f'GymPyBullet {col}', alpha=0.7)
 
-    ax.set_title(f'Motor {idx} PWM comparison')
+    ax.set_title(f'{col} comparison')
     ax.set_ylabel('PWM Value')
     ax.legend()
 
